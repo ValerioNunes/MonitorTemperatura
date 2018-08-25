f0e6ebafbc5d3e37480981d485b2d19fe60dad91
ValerioNunes <valeriobsno@gmail.com>
2018-08-25 16:01:25 -0300

    add PUT

--- src/main/java/servico/ServicoMonitorTemp.java
+++ src/main/java/servico/ServicoMonitorTemp.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -126,14 +127,26 @@ public class ServicoMonitorTemp{
   @Path("deleteTemp/{id}")
   public boolean deleteTemp(@PathParam("id") int id) {
 	  TemperaturaDAO tempDAO =  new TemperaturaDAO();
-
-	  tempDAO.Delete(id);
+	  
+	  boolean r = tempDAO.Delete(id);
 	  
 	  tempDAO.close();
-	  return true;
+	  
+	  return r;
 	  
   }
-
+  
+  @PUT
+  @Path("updateTemp/{id}")
+  @Consumes(MediaType.APPLICATION_JSON)
+  public void update(@PathParam("id") int id, Temperatura temperatura) {
+   
+	  TemperaturaDAO tempDAO =  new TemperaturaDAO();
+	  
+	  tempDAO.Update(temperatura);
+	  
+	  tempDAO.close();
+  }
 
 
 
